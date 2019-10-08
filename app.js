8ae018fa3ccd30be741e151d5f3aa3adf79560d7
Mihrab Miah <mihrabmiah13@gmail.com>
2019-10-09 02:29:44 +0600

    simple commit

--- app.js
+++ app.js
@@ -1,43 +1,39 @@
-const express = require('express');
-const nodemailer = require('nodemailer');
-const client = require('./yourClientFile')
+const express = require("express");
+const nodemailer = require("nodemailer");
+const client = require("./yourClientFile");
 const app = express();
 
-
-app.get('/', (req, res) => {
-    let smtpTransport = nodemailer.createTransport({
-        host: "smtp.gmail.com",
-        port: 465,
-        secure: true,
-        auth: {
-            type: "OAuth2",
-            user: "you_use_emailmehrabmehadi13@gmail.com",
-            clientId: yourClientFile.web.client_id,
-            clientSecret: yourClientFile.web.client_secret,
-            refreshToken: "refresh_token_from_oathplayground"
-        }
-    });
-
-    let messageTemplate = {
-        to: "reciepent_email",
-        subject: "Hello",
-        text: "Something"
+app.get("/", (req, res) => {
+  let smtpTransport = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
+    auth: {
+      type: "OAuth2",
+      user: "you_user_email",
+      clientId: yourClientFile.web.client_id,
+      clientSecret: yourClientFile.web.client_secret,
+      refreshToken: "refresh_token_from_oathplayground"
     }
+  });
 
-    smtpTransport.sendMail(messageTemplate, (err, info) => {
-        if (err) {
-            res.send(err)
-        } else {
-            smtpTransport.close();
-            return res.json({
-                status: "ok",
-                msg: "Email sent"
-            })
-        }
+  let messageTemplate = {
+    to: "recipent_email",
+    subject: "Hello",
+    text: "Something"
+  };
 
-    })
-})
+  smtpTransport.sendMail(messageTemplate, (err, info) => {
+    if (err) {
+      res.send(err);
+    } else {
+      smtpTransport.close();
+      return res.json({
+        status: "ok",
+        msg: "Email sent"
+      });
+    }
+  });
+});
 
-
-
-app.listen(3000, () => console.log('Server Up'))
+app.listen(3000, () => console.log("Server Up"));
